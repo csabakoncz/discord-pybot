a8a14f06a589776ca9518bdab7182b30b1fec031
Csaba Koncz <ckoncz@cloudera.com>
2021-04-15 22:43:12 +0200

    Hupper for restarting the process on code changes

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 # bot.py
 import os
+import sys
 
 import discord
 from dotenv import load_dotenv
+import hupper
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -18,7 +20,19 @@ async def on_message(message: discord.message.Message):
     if message.author == client.user:
         return
 
+    print('message received: %s'%message.content)
+    print('message channel=%s'%message.channel)
+    print('message author is %s'%message.author.display_name)
+
     if message.content.lower() == 'ping':
         await message.channel.send('Pong!')
 
-client.run(TOKEN)
+def main(args=sys.argv[1:]):
+    if '--reload' in args:
+        # start_reloader will only return in a monitored subprocess
+        hupper.start_reloader('bot.main')
+
+    client.run(TOKEN)
+
+if __name__ == '__main__':
+    main()
